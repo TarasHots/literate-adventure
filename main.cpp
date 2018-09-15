44ecf57f2be31c801dc093e1f69fbca70f7aa0fc
Taras.Hots <mgenny954@gmail.com>
2018-09-15 21:09:36 +0300

    Added fire image and boss shooting

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
@@ -6,24 +8,33 @@
 using namespace std;
 
 int main() {
-    const uint windowWidth = 800, height = 480;
+    const uint windowWidth = 800, windowHeight = 480;
 
-    sf::RenderWindow window(sf::VideoMode(windowWidth, height), "Obliterate!");
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Obliterate!");
 
-    sf::Texture background, bossTexture;
-    if (!background.loadFromFile("../resources/background.jpg")) {
+    sf::Texture backgroundTexture, bossTexture, fireTexture;
+    sf::Sprite backgroundSprite, bossSprite;
+
+    sf::RectangleShape infoPanel(sf::Vector2f(120, 80));
+    infoPanel.setPosition(0, 450);
+
+    std::vector<sf::Sprite> fireVector;
+
+
+    if (!backgroundTexture.loadFromFile("../resources/background.jpg")) {
         window.close();
     }
-
-    sf::Sprite backgroundSprite, bossSprite;
-    backgroundSprite.setTexture(background);
 
     if (!bossTexture.loadFromFile("../resources/boss/generic-lost-soul-150x160.png")) {
         window.close();
     }
 
-    bossSprite.setTexture(bossTexture);
+    if (!fireTexture.loadFromFile("../resources/boss/fire-70x111.png")) {
+        window.close();
+    }
 
+    backgroundSprite.setTexture(backgroundTexture);
+    bossSprite.setTexture(bossTexture);
 
     while (window.isOpen())
     {
@@ -40,20 +51,33 @@ int main() {
 
         sf::Vector2f bossPosition = bossSprite.getPosition();
 
+
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         {
-            // left key is pressed: move our character
-            float newX = bossPosition.x;
-            newX++;
-
-            if (newX >= windowWidth) {
-                break;
-            }
-
-            bossSprite.move(sf::Vector2f(0.25, 0));//move(newX, bossPosition.y);
+            bossSprite.move(0.25, 0);
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            float newPosition = bossPosition.x - 0.25f;
+            bossSprite.setPosition(newPosition, 0);
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+            sf::Sprite fire;
+            fire.setPosition(bossPosition.x, bossPosition.y);
+            fire.setTexture(fireTexture);
+            fireVector.push_back(fire);
+        }
 
+        //update all fires that has been shot
+        for (auto &fire : fireVector) {
+            fire.move(0, 0.25);
+
+            //TODO remove unneeded fires
+
+            window.draw(fire);
+        }
+
+        //window.draw(bossPositionText);
+        window.draw(infoPanel);
         window.draw(bossSprite);
         window.display();
     }
